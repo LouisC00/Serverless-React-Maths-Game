54edcd68e2193ab3834797fefcf906307f21396d
Louis <andrewng23@proton.me>
2024-04-04 17:59:58 -0600

    init style for Navbar

---
+++ src/styled/Navbar.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledNavBar = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  padding: 20px;
+`;
+
+export const StyledNavBrand = styled.div`
+  font-size: 1.4;
+
+  & > a {
+    text-decoration: none;
+  }
+`;
+
+export const StyleNavItems = styled.ul`
+  list-style: none;
+  padding-left: 0;
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 20px;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1.2 rem;
+  transition: color 200ms;
+
+  &:hover {
+    color: #e16365;
+  }
+`;
