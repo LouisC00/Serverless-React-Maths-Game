845fbf7559634125337ef9e0d870a816f78df0b3
Louis <andrewng23@proton.me>
2024-04-04 17:34:29 -0600

    add container styled css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HighScores from "./pages/HighScores";
 import GameOver from "./pages/GameOver";
 import Game from "./pages/Game";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { Container } from "./styled/Container";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/game" element={<Game />} />
-        <Route path="/highScores" element={<HighScores />} />
-        <Route path="/gameOver" element={<GameOver />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route path="/game" element={<Game />} />
+          <Route path="/highScores" element={<HighScores />} />
+          <Route path="/gameOver" element={<GameOver />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
