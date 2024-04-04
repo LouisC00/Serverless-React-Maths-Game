cd12b571b3cf1f723bfdc81ca4e75aed756f3ae9
Louis <andrewng23@proton.me>
2024-04-03 18:52:55 -0600

    create route to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
+import Game from "./pages/Game";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/game" element={<Game />} />
+        <Route path="/highScores" element={<HighScores />} />
+        <Route path="/gameOver" element={<GameOver />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
