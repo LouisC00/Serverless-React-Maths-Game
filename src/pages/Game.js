1bfe3ec466a0c7069ea20ef21833dedfea0c0cdc
Louis <andrewng23@proton.me>
2024-04-04 18:35:50 -0600

    add score, character and time and style

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,22 @@
+import {
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+  StyledCharacter,
+} from "../styled/Game";
+
+import { StrongText } from "../styled/StrongText";
+
 export default function Game() {
   return (
-    <div>
-      <h1>Game</h1>
-    </div>
+    <StyledGame>
+      <StyledScore>
+        Score:<StrongText>0</StrongText>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time: <StrongText>00: 000</StrongText>
+      </StyledTimer>
+    </StyledGame>
   );
 }
