1057189e63fb509df8f4cbd70f34b0f5bbea3681
Louis <andrewng23@proton.me>
2024-04-05 17:00:17 -0600

    add count down time and game over

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import {
   StyledGame,
   StyledScore,
@@ -7,15 +9,47 @@ import {
 
 import { StrongText } from "../styled/StrongText";
 
+const addLeadingZeros = (num, length) => {
+  return String(num).padStart(length, "0");
+};
+
 export default function Game() {
+  const [score, setScore] = useState(1);
+  const MAX_SECOND = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const navigate = useNavigate(); // Use the useNavigate hook
+
+  useEffect(() => {
+    const startTime = new Date();
+    const interval = setInterval(() => {
+      const endTime = new Date();
+      const msPassed = endTime.getTime() - startTime.getTime();
+      const remainingSeconds = MAX_SECOND - Math.floor(msPassed / 1000);
+      const remainingMs = 1000 - (msPassed % 1000);
+      setSeconds(remainingSeconds);
+      setMs(remainingMs);
+    }, 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      navigate("/gameOver"); // Use navigate instead of history.push
+    }
+  }, [seconds, ms, navigate]);
+
   return (
     <StyledGame>
       <StyledScore>
-        Score:<StrongText>0</StrongText>
+        Score:<StrongText>{score}</StrongText>
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time: <StrongText>00: 000</StrongText>
+        Time:{" "}
+        <StrongText>
+          {addLeadingZeros(seconds, 2)}: {addLeadingZeros(ms, 3)}
+        </StrongText>
       </StyledTimer>
     </StyledGame>
   );
