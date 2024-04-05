8720dd6f8ff18015f0a32e5697a30553bee6e736
Louis <andrewng23@proton.me>
2024-04-04 18:13:33 -0600

    add style to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
+import CallToAction from "../styled/CallToAction";
+import { Accent } from "../styled/Accent";
+import { StyledTitle } from "../styled/Title";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <StyledTitle>Ready to Type?</StyledTitle>
+      <CallToAction to="/game">
+        Click or type <Accent>'s'</Accent> Click to start playing!
+      </CallToAction>
     </div>
   );
 }
