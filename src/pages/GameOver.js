2772baa2621f19b0c083bc9471cb5b07c98b5447
Louis <andrewng23@proton.me>
2024-04-05 19:53:28 -0600

    show score and navigate options

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useScore } from "../context/ScoreContext";
+import { StyledLink } from "../styled/Navbar";
+
 export default function GameOver() {
+  const navigate = useNavigate();
+
+  const [score] = useScore();
+
+  if (score === -1) {
+    navigate("/");
+  }
   return (
     <div>
       <h1>Game Over</h1>
+      <p>{score}</p>
+      <StyledLink to="/">Go Home</StyledLink>
+      <StyledLink to="/game">Play Again</StyledLink>
     </div>
   );
 }
