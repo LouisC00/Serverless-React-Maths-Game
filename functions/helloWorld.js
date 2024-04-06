a71560b546d3b65fad0535096a63fb1dde4f6fee
Louis <andrewng23@proton.me>
2024-04-06 15:12:57 -0600

    use async to return functino

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,8 +1,8 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
+exports.handler = async (event, context, callback) => {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       msg: "Hello World!",
     }),
-  });
+  };
 };
