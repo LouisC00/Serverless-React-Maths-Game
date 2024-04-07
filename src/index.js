dcdf9f3d3e9eec73b78fa20af0b4c784662246bc
Louis <andrewng23@proton.me>
2024-04-06 20:58:16 -0600

    wrap app by auth0 provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ScoreProvider } from "./context/ScoreContext";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ScoreProvider>
-      <App />
-    </ScoreProvider>
+    <Auth0Provider
+      domain="dev-o6sw3kp8b583p1sk.us.auth0.com"
+      clientId="nHT6rGAdhvV0Cab34w03g00HPaWBbQqw"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <ScoreProvider>
+        <App />
+      </ScoreProvider>
+    </Auth0Provider>
+    ,
   </React.StrictMode>
 );
 
