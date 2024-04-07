31809cc6ae2035c57c5ce0c878575fc84dc66045
Louis <andrewng23@proton.me>
2024-04-06 20:57:23 -0600

    add auth0 loading checking

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ import Navbar from "./components/Navbar";
 import { Container } from "./styled/Container";
 import { Main } from "./styled/Main";
 import Global from "./styled/Global";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
   return (
     <Router>
       <Global />
