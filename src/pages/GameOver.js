050ded8ad7bcded444c2311c9d5d8de40d518032
Louis <andrewng23@proton.me>
2024-04-07 16:38:43 -0600

    add checking authorization to submit high score

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -4,12 +4,14 @@ import { StyledLink } from "../styled/Navbar";
 import { StyledCharacter } from "../styled/Game";
 import { StyledTitle } from "../styled/Title";
 import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function GameOver() {
   const navigate = useNavigate();
 
   const [score] = useScore();
   const [scoreMessage, setScoreMessage] = useState("");
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
 
   if (score === -1) {
     navigate("/");
@@ -18,9 +20,13 @@ export default function GameOver() {
   useEffect(() => {
     const saveHighScore = async () => {
       try {
+        const token = await getAccessTokenSilently();
         const options = {
           method: "POST",
           body: JSON.stringify({ name: "James", score }),
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         };
         const res = await fetch("/.netlify/functions/saveHighScore", options);
         const data = await res.json();
@@ -34,12 +40,18 @@ export default function GameOver() {
         console.error(err);
       }
     };
-    saveHighScore();
-  }, [score]);
+    if (isAuthenticated) {
+      saveHighScore();
+    }
+  }, [getAccessTokenSilently, isAuthenticated, score]);
+
   return (
     <div>
       <StyledTitle>Game Over</StyledTitle>
       <h2>{scoreMessage}</h2>
+      {!isAuthenticated && (
+        <h2>You should log in or sign up to compete for high scores!</h2>
+      )}
       <StyledCharacter>{score}</StyledCharacter>
       <div>
         <StyledLink to="/">Go Home</StyledLink>
