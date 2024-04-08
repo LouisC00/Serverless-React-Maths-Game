2f31f3a1e7bc5f7bc926560756ab29745c85ccd6
Louis <andrewng23@proton.me>
2024-04-07 18:50:46 -0600

    add dark theme on global

--- src/styled/Global.js
+++ src/styled/Global.js
@@ -1,16 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
+const isDarkThemeEnabled = false;
 export default createGlobalStyle`
-    *{
-        box-sizing: border;
-        color: #333;
-        margin: 0;
-        font-family: sans-serif;
-        font-weight: 300
-    }
 
-    h1, h2 {
-        margin-bottom: 2rem;
-        
-    }
+:root {
+    --main-bg-color: ${isDarkThemeEnabled ? "#333" : "#f9f9f9"};
+    --main-text-color: ${isDarkThemeEnabled ? "#f9f9f9" : "#333"};
+    --accent-color: #e16365;
+}
+
+* {
+    box-sizing: border-box;
+    color: var(--main-text-color);
+    margin: 0;
+    font-family: sans-serif;
+    font-weight: 300;
+}
+
+body {
+    background-color: var(--main-bg-color);
+}
+
+h1, h2 {
+    margin-bottom: 2rem;
+}
 `;
