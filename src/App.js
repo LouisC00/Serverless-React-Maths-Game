bedc8adcbe978e82c06b168bff81733a9154c967
Louis <andrewng23@proton.me>
2024-04-07 19:05:37 -0600

    use theme provider

--- src/App.js
+++ src/App.js
@@ -4,31 +4,38 @@ import GameOver from "./pages/GameOver";
 import Game from "./pages/Game";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Container } from "./styled/Container";
 import { Main } from "./styled/Main";
 import { GlobalStyle } from "./styled/Global";
-import { useAuth0 } from "@auth0/auth0-react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styled/Themes";
 
 function App() {
   const { isLoading } = useAuth0();
 
+  const theme = "light";
+  const currentTheme = theme === "light" ? lightTheme : darkTheme;
+
   return (
     <Router>
-      <GlobalStyle />
-      <Main>
-        {isLoading && <p>Loading ...</p>}
-        {!isLoading && (
-          <Container>
-            <Navbar />
-            <Routes>
-              <Route path="/game" element={<Game />} />
-              <Route path="/highScores" element={<HighScores />} />
-              <Route path="/gameOver" element={<GameOver />} />
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </Container>
-        )}
-      </Main>
+      <ThemeProvider theme={currentTheme}>
+        <GlobalStyle />
+        <Main>
+          {isLoading && <p>Loading ...</p>}
+          {!isLoading && (
+            <Container>
+              <Navbar />
+              <Routes>
+                <Route path="/game" element={<Game />} />
+                <Route path="/highScores" element={<HighScores />} />
+                <Route path="/gameOver" element={<GameOver />} />
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </Container>
+          )}
+        </Main>
+      </ThemeProvider>
     </Router>
   );
 }
