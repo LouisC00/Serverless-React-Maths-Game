4fadead22c065bbf2719b2bcc2e089f26be9cc06
Louis <andrewng23@proton.me>
2024-04-07 18:56:21 -0600

    delete loading code

--- src/App.js
+++ src/App.js
@@ -6,22 +6,15 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { Container } from "./styled/Container";
 import { Main } from "./styled/Main";
-import Global from "./styled/Global";
+import { GlobalStyle } from "./styled/Global";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { isLoading } = useAuth0();
 
-  if (isLoading) {
-    return (
-      <Main>
-        <p>loading...</p>
-      </Main>
-    );
-  }
   return (
     <Router>
-      <Global />
+      <GlobalStyle />
       <Main>
         {isLoading && <p>Loading ...</p>}
         {!isLoading && (
