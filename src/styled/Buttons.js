494ae60f476526555b9f7c7787f997122fd9a817
Louis <andrewng23@proton.me>
2024-04-07 19:54:46 -0600

    init toggle button style

---
+++ src/styled/Buttons.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background: var(--main-text-color);
+  color: var(--main-bg-color);
+  font-size: 0.6rem;
+  cursor: pointer;
+  border-radius: 4px;
+`;
