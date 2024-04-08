26201155ebf56e5b71dacc36b1db1e50fec0f808
Louis <andrewng23@proton.me>
2024-04-07 18:21:16 -0600

    add checking token validity

--- functions/utils/auth.js
+++ functions/utils/auth.js
@@ -1,3 +1,11 @@
+const jwt = require("jsonwebtoken");
+const jwks = require("jwks-rsa");
+const jwksClient = jwks({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  audience: process.env.AUTH0_AUDIENCE,
+});
+const { promisify } = require("util");
+
 const getAccessTokenFromHeaders = (headers) => {
   const rawAuthorization = headers.authorization;
 
@@ -15,6 +23,27 @@ const getAccessTokenFromHeaders = (headers) => {
   return accessToken;
 };
 
+const validateAccessToken = async (token) => {
+  try {
+    const decodedToken = jwt.decode(token, { complete: true });
+    if (!decodedToken) {
+      throw new Error("Invalid token");
+    }
+    const kid = decodedToken.header.kid;
+    const getSigningKey = promisify(jwksClient.getSigningKey);
+    const key = await getSigningKey(kid);
+    const signingKey = key.publicKey;
+
+    const options = { algorithms: "RS256" };
+    jwt.verify(token, signingKey, options);
+    return decodedToken.payload;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
 module.exports = {
   getAccessTokenFromHeaders,
+  validateAccessToken,
 };
