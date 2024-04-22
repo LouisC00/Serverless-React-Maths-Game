d2fb10115f2becfdebf1e21482c2a6ec5d336778
Louis <louisc00@proton.me>
2024-04-21 19:43:38 -0600

    initialize score to zero

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -137,7 +137,7 @@ export default function Game() {
   const navigate = useNavigate();
 
   const [cards, setCards] = useState(
-    Array.from({ length: numCards }, () => generateCard(360))
+    Array.from({ length: numCards }, () => generateCard(0))
   );
   const [hearts, setHearts] = useState();
   const [score, setScore] = useScore();
